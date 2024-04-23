d1cd08f684b2f8088b3d97758b3a42120f4c7fa6
Delau Kursovuiy <FuNFoBzzz2@mail.ru>
2024-04-23 15:19:42 +0700

    otobrajenie uspeshno

--- app/src/main/java/com/example/myapplication/Fragment/List/ListAdapter.kt
+++ app/src/main/java/com/example/myapplication/Fragment/List/ListAdapter.kt
@@ -4,33 +4,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.DB.Recipe
 import com.example.myapplication.R
 
-class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>(){
-    private  var recipelist = emptyList<Recipe>()
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+class ListAdapter(private var recipeList: List<Recipe>) : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtName: TextView = itemView.findViewById(R.id.txtname)
+        val txtDescription: TextView = itemView.findViewById(R.id.txtdiscription)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
-    }
 
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_row , parent, false)
+
+        return MyViewHolder(view)
+
+    }
 
     override fun getItemCount(): Int {
-        return recipelist.size
+        return recipeList.size
     }
-
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = recipelist[position]
-        holder.itemView.findViewById<EditText>(R.id.txtname).setText(currentItem.RecipeName)
-        holder.itemView.findViewById<EditText>(R.id.txtdiscription).setText(currentItem.Discription)
+        val currentItem = recipeList[position]
+        holder.itemView.findViewById<TextView>(R.id.txtname).setText(currentItem.RecipeName)
+        holder.itemView.findViewById<TextView>(R.id.txtdiscription).setText(currentItem.Discription)
     }
-    fun setData(recipe: List<Recipe>){
-        this.recipelist = recipe
+    fun setData(recipe: List<Recipe>) {
+        this.recipeList = recipe
         notifyDataSetChanged()
     }
-
 }
--- app/src/main/java/com/example/myapplication/Fragment/List/ListFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragment/List/ListFragment.kt
@@ -1,13 +1,22 @@
 package com.example.myapplication.Fragment.List
 
+
 import android.os.Bundle
+
 import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
+
 import android.view.View
+
 import android.view.ViewGroup
+
 import android.widget.Button
+
 import androidx.lifecycle.Observer
+
 import androidx.lifecycle.ViewModelProvider
+
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,29 +25,25 @@ import com.example.myapplication.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
-
     private lateinit var recipeViewModel: RecipeViewModel
+    private lateinit var adapter: ListAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
-
-        val adapter = ListAdapter()
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        val adapter = ListAdapter(emptyList())
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerView.adapter
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
         recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
         recipeViewModel.readAllRecipe.observe(viewLifecycleOwner, Observer { recipe ->
             adapter.setData(recipe)
         })
-
-        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
         return view
     }
-
 }
